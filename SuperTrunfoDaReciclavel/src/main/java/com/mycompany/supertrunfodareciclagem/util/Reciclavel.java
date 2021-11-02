c88b5003e2084529b6b6360538584c05b5214578
iyisakuma <yujisakuma224@gmail.com>
2021-11-02 13:54:28 -0300

    Classes iniciais

---
+++ SuperTrunfoDaReciclavel/src/main/java/com/mycompany/supertrunfodareciclagem/util/Reciclavel.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.supertrunfodareciclagem.util;
+
+/**
+ *
+ * @author iyiSakuma
+ */
+public class Reciclavel extends Carta{
+
+    @Override
+    public boolean isReciclavel() {
+       return true;
+    }
+
+    
+}
